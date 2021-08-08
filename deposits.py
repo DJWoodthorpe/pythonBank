cd33880211824c7e27b1ca73b0704fb5f7b81f19
Daniel Woodthorpe <Kycerian@gmail.com>
2021-08-08 19:53:25 +0800

    first commit

---
+++ deposits.py
@@ -0,0 +1,10 @@
+class deposits:
+
+    import time
+
+    def __init__(self, timeStamp, amount, customer, account):
+        self.amount = amount
+        self.customer = customer
+        self.account = account
+        self.timeStamp = 
+
